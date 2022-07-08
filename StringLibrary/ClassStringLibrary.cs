a119a2bced2001265982baed42e345965b844319
hsst hanwa <thongchai.worktte@gmail.com>
2022-07-08 08:35:01 +0700

    Add project files.

---
+++ StringLibrary/ClassStringLibrary.cs
@@ -0,0 +1,16 @@
+﻿namespace StringLibrary
+{
+    public static class ClassStringLibrary
+    {
+        public static bool StartsWithUpper(this string? str)
+        {
+            if(string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+
+        }
+
+    }
+}
